88e36e3f6f706878f366314d3a84aaa583fd8013
kangjinmo <ilovejs97@nate.com>
2017-02-06 15:53:23 +0900

    update

--- app.js
+++ app.js
@@ -16,9 +16,10 @@ app.get('/reset', function (req,res){
 });
 app.get('/set/count', function (req,res) {
   if(req.query.count) data.count=req.query.count;
+  res.render('my_first_ejs',data);
 });
 app.get('/set/:num', function (req,res) {
-  data.count=req.parans.num;
+  data.count=req.params.num;
   res.render('my_first_ejs',data);
 });
 
