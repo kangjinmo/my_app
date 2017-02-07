ba58bd167b134cdcca46f010b60cefaea60af0de
kangjinmo <ilovejs97@nate.com>
2017-02-07 10:04:31 +0900

    update

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-var express  = require('express');
-var app      = express();
-var path     = require('path');
+// import modules
+var express = require('express');
+var app = express();
+var path = require('path');
 var mongoose = require('mongoose');
-var session  = require('express-session');
-var flash    = require('connect-flash');
-var bodyParser     = require('body-parser');
+var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
-// database
+// connect database
 mongoose.connect(process.env.MONGO_DB);
 var db = mongoose.connection;
 db.once("open",function () {
@@ -17,29 +16,68 @@ db.on("error",function (err) {
   console.log("DB ERROR :", err);
 });
 
-// view engine
+// model setting
+var postSchema = mongoose.Schema({
+  title: {type:String, required:true},
+  body: {type:String, required:true},
+  createdAt: {type:Date, default:Date.now},
+  updatedAt: Date
+});
+var Post = mongoose.model('post',postSchema);
+
+// view setting
 app.set("view engine", 'ejs');
 
-// middlewares
+// set middlewares
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
-app.use(flash());
-app.use(session({secret:'MySecret'}));
 
-// passport
-var passport = require('./config/passport');
-app.use(passport.initialize());
-app.use(passport.session());
-
-// routes
-app.use('/', require('./routes/home'));
-app.use('/users', require('./routes/users'));
-app.use('/posts', require('./routes/posts'));
+// set routes
+app.get('/posts', function(req,res){
+  Post.find({}).sort('-createdAt').exec(function (err,posts) {
+    if(err) return res.json({success:false, message:err});
+    res.render("posts/index", {data:posts});
+  });
+}); // index
+app.get('/posts/new', function(req,res){
+  res.render("posts/new");
+}); // new
+app.post('/posts', function(req,res){
+  console.log(req.body);
+  Post.create(req.body.post,function (err,post) {
+    if(err) return res.json({success:false, message:err});
+    res.redirect('/posts');
+  });
+}); // create
+app.get('/posts/:id', function(req,res){
+  Post.findById(req.params.id, function (err,post) {
+    if(err) return res.json({success:false, message:err});
+    res.render("posts/show", {data:post});
+  });
+}); // show
+app.get('/posts/:id/edit', function(req,res){
+  Post.findById(req.params.id, function (err,post) {
+    if(err) return res.json({success:false, message:err});
+    res.render("posts/edit", {data:post});
+  });
+}); // edit
+app.put('/posts/:id', function(req,res){
+  req.body.post.updatedAt=Date.now();
+  Post.findByIdAndUpdate(req.params.id, req.body.post, function (err,post) {
+    if(err) return res.json({success:false, message:err});
+    res.redirect('/posts/'+req.params.id);
+  });
+}); //update
+app.delete('/posts/:id', function(req,res){
+  Post.findByIdAndRemove(req.params.id, function (err,post) {
+    if(err) return res.json({success:false, message:err});
+    res.redirect('/posts');
+  });
+}); //destroy
 
 // start server
-var port = process.env.PORT || 3000;
-app.listen(port, function(){
+app.listen(3000, function(){
   console.log('Server On!');
 });
